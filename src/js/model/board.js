20a088c577b2b3e993c74a1a39fd3481e7444934
Ryan McGuinness <ryan@fallingonline.com>
2023-09-23 17:27:49 -0400

    Fixed board syntax

--- src/js/model/board.js
+++ src/js/model/board.js
@@ -4,9 +4,9 @@ export class Board  {
 
     show() {
             let total = 100;
-            for (let i : number = 10; i >= 1; i--) {
+            for (let i = 10; i >= 1; i--) {
                 let row = []
-                for (let j : number = 1; j<=10; j++) {
+                for (let j = 1; j<=10; j++) {
                     row.push(total--);
                 }
                 row = (i%2==0) ? row : row.reverse()
